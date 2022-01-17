65e89b134449e37aee2f61cdbee14c1e2827cff1
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2022-01-17 09:36:04 -0300

    Initial commit

---
+++ SmartEnumExample/Subscriptions.cs
@@ -0,0 +1,42 @@
+﻿using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartEnumExample
+{
+    public abstract class Subscriptions : SmartEnum<Subscriptions>
+    {
+        public static readonly Subscriptions Free = new FreeTier();
+        public static readonly Subscriptions Premium = new PremiumTier();
+        public abstract double Discount { get; }
+
+        public Subscriptions(string name, int value) : base(name, value)
+        {
+
+        }
+
+        private sealed class FreeTier : Subscriptions
+        {
+            public FreeTier() : base(nameof(Free), 1)
+            {
+            }
+
+            public override double Discount => .0;
+        }
+
+        private sealed class PremiumTier : Subscriptions
+        {
+            public PremiumTier() : base(nameof(Premium), 2)
+            {
+            }
+            public override double Discount => .25;
+        }
+
+
+
+
+    }
+}
